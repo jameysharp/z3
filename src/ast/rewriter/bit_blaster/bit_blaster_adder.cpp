726b58fc961aad0df55eaf5fcd74a29c137be8e4
Jamey Sharp <jamey@minilop.net>
2021-08-21 17:01:06 -0700

    use Dadda tree-structured adder

--- src/ast/rewriter/bit_blaster/bit_blaster_adder.cpp
+++ src/ast/rewriter/bit_blaster/bit_blaster_adder.cpp
@@ -16,61 +16,148 @@ bit_blaster_adder::bit_blaster_adder(bool_rewriter & rewriter, unsigned sz, expr
         add_bit(i, bits[i]);
 }
 
+// Compute the next height to reduce the input columns to, given the current
+// maximum height, as defined by Luigi Dadda in "Some schemes for parallel
+// multipliers", 1965.
+//
+// https://oeis.org/A061418
+// https://en.wikipedia.org/wiki/Dadda_multiplier
+//
+// This sequence is defined here as:
+//
+// d(0) = 1
+// d(1) = 2
+// d(k) = floor(1.5 * d(k-1))
+//
+// The d(0) element is not in the conventional definition; it doesn't fit the
+// nice recurrence relation, since floor(1.5 * 1) = 1. But including it means
+// the caller doesn't need to special-case the final stage of adders.
+static unsigned dadda_limit(unsigned height) {
+    // Prevent overflow---though if you add up 2.8 billion bit-blasted numbers,
+    // you probably run out of memory before hitting this limit.
+    SASSERT(height <= UINT_MAX / 3 * 2);
+
+    // Assuming unsigned is 32-bit, this loop can't run more than 53 times. It
+    // shouldn't touch memory as long as your CPU has at least four registers.
+    // So just recompute the sequence every time we need it.
+    unsigned x = 1, y = 2;
+    while (y < height) {
+        x = y;
+        // equivalent to y = y * 3 / 2 but doesn't overflow prematurely
+        y += y >> 1;
+    }
+    return x;
+}
+
 expr_ref bit_blaster_adder::sum_bits(vector< expr_ref_vector > & columns, expr_ref_vector & out_bits) const {
     SASSERT(out_bits.empty());
 
-    expr_ref_vector carries(m());
+    unsigned height = 0;
+    for (auto & column : columns)
+        if (height < column.size())
+            height = column.size();
+
+    expr_ref_vector cur(m()), next(m());
     expr_ref tmp1(m()), tmp2(m()), tmp3(m());
+    expr_ref carry(m());
+    carry = m().mk_false();
 
-    for (auto & column : columns) {
-        column.append(carries);
-        carries.reset();
+    while (height > 1) {
+        // In each iteration, reduce the height of every column that has more
+        // bits than the current Dadda limit until it has exactly the right
+        // number of bits. This can add carry bits to the next column, which
+        // may then need to be reduced as well.
+        height = dadda_limit(height);
 
-        while (column.size() >= 3) {
-            expr * a = column.back();
-            column.pop_back();
-            expr * b = column.back();
-            column.pop_back();
-            expr * c = column.back();
-            column.pop_back();
+        SASSERT(cur.empty());
+        for (auto & column : columns) {
+            SASSERT(next.empty());
 
-            m_rewriter.mk_xor(a, b, tmp1);
-            m_rewriter.mk_xor(tmp1, c, tmp2);
-            column.push_back(tmp2);
+            // Reduce carry bits from the previous column first, followed by
+            // any bits which were already in this column.
+            if (cur.empty()) {
+                cur.swap(column);
+            } else {
+                cur.append(column);
+                column.reset();
+            }
 
-            m_rewriter.mk_and(a, b, tmp2);
-            m_rewriter.mk_and(tmp1, c, tmp3);
-            // tmp2 and tmp3 can't be true at the same time, so use
-            // whichever of mk_or vs mk_xor makes the most sense here.
-            m_rewriter.mk_or(tmp2, tmp3, tmp1);
-            carries.push_back(tmp1);
+            if (cur.size() <= height) {
+                cur.swap(column);
+                continue;
+            }
+
+            unsigned excess = cur.size() - height;
+
+            // A full adder replaces three bits with one, reducing the excess
+            // by two. Do that until we don't have two excess bits left to
+            // reduce. If there's one excess bit left at that point, use a half
+            // adder to replace two bits with one. So the number of new bits is
+            // half the excess, rounded up.
+            const unsigned changed = (excess + 1) / 2;
+
+            // To make progress, we'd better be generating some new bits.
+            SASSERT(changed > 0);
+
+            // We only replace enough bits in each round to get down to the
+            // height target; the rest pass through to the next round
+            // unchanged.
+            const unsigned unchanged = height - changed;
+            for (unsigned i = cur.size() - unchanged; i < cur.size(); i++)
+                column.push_back(cur.get(i));
+
+            unsigned i = 0;
+            while (excess >= 2) {
+                excess -= 2;
+                expr * a = cur.get(i++);
+                expr * b = cur.get(i++);
+                expr * c = cur.get(i++);
+
+                m_rewriter.mk_xor(a, b, tmp1);
+                m_rewriter.mk_xor(tmp1, c, tmp2);
+                column.push_back(tmp2);
+
+                m_rewriter.mk_and(a, b, tmp2);
+                m_rewriter.mk_and(tmp1, c, tmp3);
+                // tmp2 and tmp3 can't be true at the same time, so use
+                // whichever of mk_or vs mk_xor makes the most sense here.
+                m_rewriter.mk_or(tmp2, tmp3, tmp1);
+                next.push_back(tmp1);
+            }
+
+            if (excess) {
+                expr * a = cur.get(i++);
+                expr * b = cur.get(i++);
+
+                m_rewriter.mk_xor(a, b, tmp1);
+                column.push_back(tmp1);
+
+                m_rewriter.mk_and(a, b, tmp1);
+                next.push_back(tmp1);
+            }
+
+            SASSERT(i == cur.size() - unchanged);
+            SASSERT(column.size() == height);
+
+            cur.reset();
+            next.swap(cur);
         }
 
-        if (column.size() == 2) {
-            expr * a = column.back();
-            column.pop_back();
-            expr * b = column.back();
-            column.pop_back();
-
-            m_rewriter.mk_xor(a, b, tmp1);
-            column.push_back(tmp1);
-
-            m_rewriter.mk_and(a, b, tmp1);
-            carries.push_back(tmp1);
+        for (auto & bit : cur) {
+            m_rewriter.mk_xor(carry, bit, tmp1);
+            carry = tmp1;
         }
-
-        out_bits.push_back(column.get(0, m().mk_false()));
+        cur.reset();
     }
 
+    // At this point, the height of every column is 0 or 1, so generating the
+    // output bits is easy.
+    for (auto & column : columns)
+        out_bits.push_back(column.get(0, m().mk_false()));
     SASSERT(out_bits.size() == size());
 
     // We return the carry separately in case the caller wants it.
-    tmp1 = m().mk_false();
-    for (auto & carry : carries) {
-        m_rewriter.mk_xor(tmp1, carry, tmp2);
-        tmp1 = tmp2;
-    }
-    return tmp1;
+    return carry;
 }
 
 expr_ref bit_blaster_adder::variable_bits(expr_ref_vector & out_bits) const {
